1519b5843215e67e056ee3e872ef92880e06ef5f
Jude Thaddeau Data <78249269+Zero-Luminance@users.noreply.github.com>
2022-05-21 19:36:48 +1000

    Initial commit for circular linked list (CLL) implementation.

---
+++ data-structures/linked-lists/circular-linked-list.c
@@ -0,0 +1,10 @@
+/**
+ * @file    circular-linked-list.c
+ * @brief   A file for circular linked list (CLL) implementation
+ * @author  Jude Thaddeau Data (a.k.a Zero Luminance)
+ * Github:  https://github.com/Zero-Luminance
+ * @note    Before using this file REMEMBER to replace ALL instances of
+ *          'void' associated with cll_node_t's 'data' component with
+ *          desired data type.
+*/
+/* -------------------------------------------------------------------------- */
---
+++ data-structures/linked-lists/circular-linked-list.h
@@ -0,0 +1,10 @@
+/**
+ * @file    circular-linked-list.h
+ * @brief   A header file for circular linked list (CLL) implementation
+ * @author  Jude Thaddeau Data (a.k.a Zero Luminance)
+ * Github:  https://github.com/Zero-Luminance
+ * @note    Before using this file REMEMBER to replace ALL instances of
+ *          'void' associated with cll_node_t's 'data' component with
+ *          desired data type.
+*/
+/* -------------------------------------------------------------------------- */
