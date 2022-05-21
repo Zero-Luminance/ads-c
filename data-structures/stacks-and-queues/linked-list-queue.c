bc2e42535e9e3909bbcc7b7a8be9ffe74832b987
Jude Thaddeau Data <78249269+Zero-Luminance@users.noreply.github.com>
2022-05-21 19:35:57 +1000

    Initial commit for linked list stacks & queues implementation.

---
+++ data-structures/stacks-and-queues/linked-list-queue.c
@@ -0,0 +1,10 @@
+/**
+ * @file    linked-list-queue.c
+ * @brief   A file for linked list queue (LLQ) implementation
+ * @author  Jude Thaddeau Data (a.k.a Zero Luminance)
+ * Github:  https://github.com/Zero-Luminance
+ * @note    Before using this file REMEMBER to replace ALL instances of
+ *          'void' associated with llq_node_t's 'data' component with
+ *          desired data type.
+*/
+/* -------------------------------------------------------------------------- */
---
+++ data-structures/stacks-and-queues/linked-list-queue.h
@@ -0,0 +1,10 @@
+/**
+ * @file    linked-list-queue.h
+ * @brief   A header file for linked list queue (LLQ) implementation
+ * @author  Jude Thaddeau Data (a.k.a Zero Luminance)
+ * Github:  https://github.com/Zero-Luminance
+ * @note    Before using this file REMEMBER to replace ALL instances of
+ *          'void' associated with llq_node_t's 'data' component with
+ *          desired data type.
+*/
+/* -------------------------------------------------------------------------- */
