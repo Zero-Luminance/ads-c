b80b09a2ae477edd4203ecf9f4781eef23ef7471
Jude Thaddeau Data <78249269+Zero-Luminance@users.noreply.github.com>
2022-06-10 12:49:07 +1000

    Changed names from lls -> slls.

--- data-structures/stacks-and-queues/singly-linked-list-stack-files/singly-linked-list-stack.c
+++ data-structures/stacks-and-queues/singly-linked-list-stack-files/singly-linked-list-stack.c
@@ -1,10 +1,10 @@
 /**
- * @file    linked-list-stack.c
- * @brief   A file for linked list stack (LLS) implementation
+ * @file    singly-linked-list-stack.c
+ * @brief   A file for linked list stack (SLLS) implementation
  * @author  Jude Thaddeau Data (a.k.a Zero Luminance)
  * Github:  https://github.com/Zero-Luminance
  * @note    Before using this file REMEMBER to replace ALL instances of
- *          'void' associated with lls_node_t's 'data' component with
+ *          'void' associated with slls_node_t's 'data' component with
  *          desired data type.
 */
 /* -------------------------------------------------------------------------- */
