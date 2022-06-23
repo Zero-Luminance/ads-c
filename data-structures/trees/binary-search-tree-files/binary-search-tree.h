290321f678f2d685adbda4e70c844c70c4b6fdd2
Jude Thaddeau Data <78249269+Zero-Luminance@users.noreply.github.com>
2022-06-23 14:11:09 +1000

    Added prototype functions for BST operations.

--- data-structures/trees/binary-search-tree-files/binary-search-tree.h
+++ data-structures/trees/binary-search-tree-files/binary-search-tree.h
@@ -23,5 +23,30 @@ typedef struct bst_tree_t {
 /* -------------------------------------------------------------------------- */
 
 // BST Operations:
+bst_tree_t *bst_init(void);
+void bst_free(bst_tree_t *bst);
+int bst_is_empty(bst_tree_t *bst);
+
+void bst_inorder(bst_tree_t *bst, void action(void*));
+void bst_preorder(bst_tree_t *bst, void action(void*));
+void bst_postorder(bst_tree_t *bst, void action(void*));
+
+bst_node_t *bst_search(bst_tree_t *bst, void *search_key);
+static bst_node_t *bst_recursive_search(bst_node_t *root, void *search_key, int cmp(void*, void*));
+static bast_node_t *bst_iterative_search(bst_node_t *root, void *search_key, int cmp(void*, void*));
+
+bst_node_t *bst_minimum(bst_tree_t *bst);
+bst_node_t *bst_maximum(bst_tree_t *bst);
+
+bst_node_t *bst_predecessor(bst_tree_t *bst);
+bst_node_t *bst_successor(bst_tree_t *bst);
+
+bst_node_t *bst_insert(bst_tree_t *bst, void *new_key);
+static bst_node_t *bst_recursive_insert(bst_node_t *root, bst_node_t *new_node, int cmp(void*, void*));
+static bst_node_t *bst_iterative_insert(bst_node_t *root, bst_node_t *new_node, int cmp(void*, void*));
+
+void bst_transplant(bst_tree_t *bst, bst_node_t *target, bst_node_t *replacement);
+static void bst_delete(bst_tree_t *bst, void *target_key);
+static void bst_recursive_delete(bst_tree_t *bst, bst_node_t *target);
 
 /* -------------------------------------------------------------------------- */
